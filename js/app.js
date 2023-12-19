424ff59dcd65300038560e0db8fca7bd9ce25831
Joseph-Brannen <141746228+Joseph-Brannen@users.noreply.github.com>
2023-12-19 16:47:08 -0500

    Add step 6

--- js/app.js
+++ js/app.js
@@ -24,3 +24,20 @@ function getRandomPhraseAsArray(arr) {
     const characters = randomElement.split("");
     return characters;
 }
+
+function addPhraseToDisplay(arr) {
+    for ( let i = 0; i < arr.length; i++ ) {
+        const li = document.createElement('li');
+        li.textContent = arr[i];
+        const ul = document.querySelector('ul');
+        ul.append(li);
+        if ( arr[i] === 'a' || arr[i] === 'b' || arr[i] === 'c' || arr[i] === 'd' || arr[i] === 'e' || arr[i] === 'f' || arr[i] === 'g' || arr[i] === 'h' || arr[i] === 'i' || arr[i] === 'j' || arr[i] === 'k' || arr[i] === 'l' || arr[i] === 'm' || arr[i] === 'n' || arr[i] === 'o' || arr[i] === 'p' || arr[i] === 'q' || arr[i] === 'r' || arr[i] === 's' || arr[i] === 't' || arr[i] === 'u' || arr[i] === 'v' || arr[i] === 'w' || arr[i] === 'x' || arr[i] === 'y' || arr[i] === 'z' ) {
+            li.className = 'letter';
+        } else {
+            li.className = 'space';
+        }
+    }
+}
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
