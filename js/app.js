6cb1532e4e250bee6554edcfb77760743e047d87
Joseph-Brannen <141746228+Joseph-Brannen@users.noreply.github.com>
2023-12-19 11:35:56 -0500

    Add step 3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
+
+const overlay = document.getElementById('overlay');
+const startButton = document.querySelector('.btn_reset');
+
+overlay.addEventListener( 'click', (e) => {
+    if ( e.target.tagName === 'A' ) {
+        overlay.style.display = 'none';
+    }
+});
